617d459487f148d8cbebf6ea3bf4dbd41fd87d3c
Pertti Soomann <pert@e-tailwebstores.com>
2014-01-01 21:44:28 +0000

    Added software license and minor CSS fix

--- print_d.php
+++ print_d.php
@@ -1,5 +1,41 @@
 <?php
 
+/*
+
+Written by Pertti Soomann, 2014
+
+Check for latest version and report issues at
+https://github.com/vikerlane/print_d
+
+Copyright (c) 2014, Pertti Soomann @ Vikerlane
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies, 
+either expressed or implied, of the FreeBSD Project.
+
+*/
+
 function print_d($var)
 {
 	$css = array(
@@ -9,7 +45,7 @@ function print_d($var)
 		'td' => 'border: 1px solid #ddd; font-size: 11px; vertical-align: top; padding: 2px 4px 2px 4px;',
 		'method' => 'color: #00d;',
 		'attributes' => 'padding: 2px;',
-		'table-attributes' => 'width: 100%;',
+		'table-attributes' => 'width: 100%; border-collapse:collapse;',
 		'required' => 'text-align: center; color: #d00; width: 16px;',
 		'attribute' => 'width:120px;',
 		'pre' => 'font-size: 11px !important; margin: 0; padding: 0; background: none; border: 0;',
