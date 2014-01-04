ff1c2467f3cbd613bbe2db5ef5caf82f8a71b66c
Pertti Soomann <pert@e-tailwebstores.com>
2014-01-04 12:58:57 +0000

    Updated examples

--- examples.php
+++ examples.php
@@ -9,7 +9,9 @@
 	echo print_d(1);
 	echo print_d(1.1);
 	echo print_d(true);
+	echo print_d(null);
 	echo print_d(array(1, 2, 3));
+	echo print_d(array());
 
 	echo '<br style="clear:both;">';
 	echo '<h3>Printing variables</h3>';
@@ -20,8 +22,18 @@
 	$string = 'Variable string';
 	echo print_d($string);
 
+	$null = null;
+	echo print_d($null);
+
 	$array = array(1, 'Two', 3, '4');
 	echo print_d($array);
+
+	$array2 = array('a' => 1, 'b' => 2, 'c' => 'D');
+	echo print_d($array2);
+	echo print_d($array2['a']);
+
+	$empty_array = array();
+	echo print_d($empty_array);
 
 	echo '<br style="clear:both;">';
 	echo '<h3>Printing classes</h3>';
@@ -34,18 +46,45 @@
 	class Test
 	{
 		var $var1 = 'Var 1 value',
-			$var2 = 'Var 2 value';
+			$var2 = 'Var 2 value',
+			$var3 = null;
 
 		function method1()
 		{
-
+			return 'from method';
 		}
 
 		function method2($value, $value2 = 'Default value set')
 		{
-
+			return array(1, 2, 3);
 		}
 	}
 
 	$testclass = new Test;
 	echo print_d($testclass);
+	echo print_d($testclass, true);
+
+	echo print_d(new stdclass);
+
+	echo '<br style="clear:both;">';
+	echo '<h3>Printing multidimensional arrays</h3>';
+
+	$m_array = array(
+		array(1, 2, 3),
+		array('one', 'two', 'three'),
+		4,
+		'five',
+		(object) array('a' => 'b', 'c' => 'd'),
+		array(array(1, 2, 3), array(4, 5, 6))
+	);
+	echo print_d($m_array);
+
+	echo '<br style="clear:both;">';
+	echo '<h3>Printing functions</h3>';
+
+	echo print_d(date('Y'));
+	echo print_d($testclass->method1());
+	echo print_d($testclass->method2(true));
+
+	$date = array('2014' => 'Year');
+	echo print_d($date[date('Y')]);
